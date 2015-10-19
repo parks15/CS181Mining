ef598f4a920eba645dc2cf3099a6a5f5ec7f7a05
parks15 <work@dakinwireless141-57.hampshire.edu>
2015-10-18 22:18:47 -0400

    Added Transportation project
    Added new project to repository and did some reorganizing/renaming

---
+++ Transportation Project/Transportation1/Assets/CubeBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnMouseDown() {
+		GameObject[] allCubes = GameObject.FindGameObjectsWithTag("Clickable Cube");
+		foreach (GameObject oneCube in allCubes) {
+			oneCube.GetComponent<Renderer>().material.color = Color.white;
+		}
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
